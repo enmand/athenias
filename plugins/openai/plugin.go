113809e2b033e441d827945d1447faf40a622291
Dan Enman <enmand@gmail.com>
2023-03-31 15:00:55 -0300

    chore: pass OpenAI configuration from env, init client in plugin
    ATH-2

--- plugins/openai/plugin.go
+++ plugins/openai/plugin.go
@@ -5,9 +5,14 @@ import (
 	"math/rand"
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
 	"github.com/unerror/athenais/pkg/athenais"
 	"maunium.net/go/mautrix/event"
+)
+
+var (
+	openaiKey string
 )
 
 type Plugin struct {
@@ -19,14 +24,35 @@ type Plugin struct {
 
 	// log is the logger to use for logging
 	log *zerolog.Logger
+
+	r *rand.Rand
+
+	cfg *Configuration
+}
+
+type Configuration struct {
+	// Prompt is the system Prompt to use to prime responses
+	Prompt string
+
+	// Chance is the Chance to respond to a message
+	Chance int
+
+	// APIKey is the API key for OpenAI
+	APIKey string
 }
 
 // NewPlugin creates a new OpenAI plugin
-func NewPlugin(aiClient *Client, prompt string) *Plugin {
-	rand.Seed(time.Now().UnixNano())
+func NewPlugin(cfg Configuration) *Plugin {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	aiClient := NewClient(
+		cfg.APIKey,
+		WithPrompt(cfg.Prompt),
+	)
 
 	return &Plugin{
 		client: aiClient,
+		r:      r,
+		cfg:    &cfg,
 	}
 }
 
@@ -48,25 +74,25 @@ func (p *Plugin) Init(bot *athenais.Bot, log *zerolog.Logger) {
 	)
 }
 
-func (p *Plugin) handleMessage(evt *event.Event) {
+func (p *Plugin) handleMessage(evt *event.Event) error {
 	msg := evt.Content.AsMessage()
-	p.log.Info().Interface("msg", msg).Interface("evt", evt).Msg("Received message")
-
-	if evt.Sender == p.bot.ID() {
-		p.log.Debug().Msg("Ignoring message from self")
-		return
-	}
 
 	if msg.MsgType == event.MsgText {
-		r := rand.Int() % 100
+		r := p.r.Int() % 100
 		p.log.Info().Int("r", r).Msg("Random number")
-		if r < 101 {
-			out, err := p.client.Prompt(context.Background(), evt.Content.AsMessage().Body)
+		if r < p.cfg.Chance {
+			p.log.Debug().Str("msg", msg.Body).Msg("Responding to message")
+			out, err := p.client.Prompt(context.Background(), msg.Body)
 			if err != nil {
 				p.log.Error().Err(err).Msg("Failed to generate response")
-				return
+				return errors.Wrap(err, "failed to generate response")
 			}
-			p.bot.SendText(evt.RoomID, out)
+
+			if err := p.bot.SendText(evt.RoomID, out); err != nil {
+				return errors.Wrap(err, "failed to send message")
+			}
 		}
 	}
+
+	return nil
 }
